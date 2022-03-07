d67c68ecb6c1c71f74747a4d62a80a7e4144b173
Yatham Tejasri <94678025+Sridha123@users.noreply.github.com>
2022-03-07 07:01:23 +0000

    p2 final

--- p1final.c
+++ p1final.c
@@ -1,1 +1,46 @@
 #include <stdio.h>
+void input(int *num1,int *den1,int *num2,int *den2)
+{
+  printf("Enter four numbers\n");
+  scanf("%d%d%d%d",num1,den1,num2,den2);
+}
+int gcd(int a,int b)
+{
+  int gcd=1;
+  while(b!=0)
+    {
+      gcd=a%b;
+      a=b;
+      b=gcd;
+      
+    }
+  return a;
+}
+void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
+{
+  
+}
+
+  
+
+
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
